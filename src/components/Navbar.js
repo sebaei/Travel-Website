536ef0b7eb9955fdab650ad124dbb167b566491c
sebaei <47517850+sebaei@users.noreply.github.com>
2022-11-21 03:29:11 +0200

    Add Hero section and cards to display trips

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL <i className="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
